4db776ea9fa73c80a1522de3af506a105e34ba47
Juan Sebastian Galeano A <juan_seb.galeano@uao.edu.co>
2023-03-20 18:36:43 -0500

    first commit

---
+++ src/components/Home/Header/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => (
+  <header className='home-header'>
+    <h2>Diseño Transmedia</h2>
+    <h1>
+      <span>“</span> Blog <span>”</span>
+    </h1>
+    <p>
+      Te contamos todo el recorrido <br /> de un proyecto transmedia muy interesante
+    </p>
+  </header>
+);
+
+export default Header;
